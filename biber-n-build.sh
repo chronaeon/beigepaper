050dcd259b5b1797cc48f3c7284f33d0de9eac73
chronaeon <micahdameron@gmail.com>
2018-03-09 07:39:09 -0500

    modified:   biber-n-build.sh

--- biber-n-build.sh
+++ biber-n-build.sh
@@ -1,5 +1,31 @@
-pdflatex beigepaper.tex &&
-biber beigepaper &&
-pdflatex beigepaper.tex &&
-makeindex beigepaper.idx &&
-pdflatex beigepaper.tex
+#!/bin/bash
+# Biber-N-Build, Version 2
+
+#  Warning:
+#  -------
+#  This build script requires the installation of biber, pdflatex, and the full texlive library on an Ubuntu or Debian based system to work. Most likely it can be built using the same packages on other systems, but such a build has not been tested yet.
+
+mv beigepaper.tex foopaper.bar # Temporarily rename the source file.
+
+rm beigepaper* 		       # Remove the previously built files.
+
+mv foopaper.bar beigepaper.tex # Rename the source file.
+
+pdflatex beigepaper.tex        # 1st build from source.
+
+biber beigepaper 	       # Generate bibliography.
+
+pdflatex beigepaper.tex        # 2nd build from source w/ bibliography.
+
+makeindex beigepaper.idx       # Generate index.
+
+pdflatex beigepaper.tex        # 3rd build from source w/ bibliography and index.
+
+if [ -e "beigepaper.pdf" ]     # Does beigepaper.pdf exist in this directory?
+then
+  echo "Beigepaper build successful."
+else
+  echo "Beigepaper build failed. Check write access and dependencies."
+
+exit 
+     
