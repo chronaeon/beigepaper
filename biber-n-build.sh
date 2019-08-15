bec08815db62bc799f47fa32d453362e46f6702e
Micah Dameron <micahdameron@gmail.com>
2019-08-15 09:19:09 -0600

    modified:   beigepaper.pdf 	modified:   beigepaper.tex 	modified:   biber-n-build.sh - Changed compiler from PDFLatex to LuaLaTeX to support TeX-Gyre font - Changed default font to TeX-Gyre-Heroes. Sans Fonts are easier to read in technical documents. - Changed date format to ISO - Changed version numbering to the middle of the header.

--- biber-n-build.sh
+++ biber-n-build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# Biber-N-Build, Version 3 
+# Biber-N-Build, Version 4 
 
 #  Warning:
 #  -------
-#  This build script requires the installation of biber, pdflatex, and the full texlive library on an Ubuntu or Debian based system to work. Most likely it can be built using the same packages on other systems, but such a build is presently untested.
+#  This build script requires the installation of biber, lualatex, and the full texlive library on an Ubuntu or Debian based system to work. Most likely it can be built using the same packages on other systems, but such a build is presently untested.
 
 mv beigepaper.tex foopaper.bar # Temporarily rename the source file.
 rm beigepaper* 		       # Remove the previously built files.
 mv foopaper.bar beigepaper.tex # Rename the source file.
-pdflatex beigepaper.tex        # 1st build from source.
+lualatex beigepaper.tex        # 1st build from source.
 biber beigepaper 	       # Generate bibliography.
-pdflatex beigepaper.tex        # 2nd build from source w/ bibliography.
+lualatex beigepaper.tex        # 2nd build from source w/ bibliography.
 makeindex beigepaper.idx       # Generate index.
-pdflatex beigepaper.tex        # 3rd build from source w/ bibliography and index.
+lualatex beigepaper.tex        # 3rd build from source w/ bibliography and index.
 clear
 
 if [ -e "beigepaper.pdf" ]     # Does beigepaper.pdf exist in this directory?
